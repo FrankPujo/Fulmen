cd2feac788f5eaf4240ea1292128c95d71e7ccfa
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2020-11-10 12:45:26 +0100

    Add files via upload

--- sentences.sql
+++ sentences.sql
@@ -1,10 +1,10 @@
 CREATE TABLE sentences (
-    id NUMBER;
+    id INTEGER;
     sentence TEXT
     );
 INSERT INTO sentences ( num, sentence )
 VALUES ( 1, 'Do not cover under the trees!' );
 INSERT INTO sentences ( num, sentence )
-VALUES ( 2, 'Zeus is the Greek God of lightings' );
+VALUES ( 2, 'What about Zeus and Thor fighting each other?' );
 INSERT INTO sentences ( num, sentence )
-VALUES ( 3, 'Do not belive everything you find on the internet' )
+VALUES ( 3, 'Do not belive everything you find on the internet' );
