cd1cc7e7205db254b58f962fd4aeeeff958b6b5b
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2020-11-10 09:33:32 +0100

    1st attempt to create SQL table

---
+++ sentences.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sentences (
+    id NUMBER;
+    sentence TEXT
+    );
+INSERT INTO sentences ( num, sentence )
+VALUES ( 1, 'Do not cover under the trees!' );
+INSERT INTO sentences ( num, sentence )
+VALUES ( 2, 'Zeus is the Greek God of lightings' );
+INSERT INTO sentences ( num, sentence )
+VALUES ( 3, 'Do not belive everything you find on the internet' )
