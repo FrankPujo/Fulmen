8f356ceb090d6b4963749399461d4ee48831d107
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2020-11-16 11:53:28 +0100

    Pre Menu Update

--- premenu.lua
+++ premenu.lua
@@ -1,13 +1,19 @@
 local composer = require ("composer")
 local scene = composer.newScene( premenu )
 
-local startBtn = widget.newButton( 75, 75 )
+local startBtn = widget.newButton(
+    {
+        left = 100,
+        top = 200,
+        onRelease = onStartRel
+    }
+)
 
 local circle1 = display.newCircle( 75, 75 )
 circle1.stroke = 3
 circle:setStrokeColor( 0.1, 0.2, 0.8 )
 
-local function onStartRel( listener )
+local function onStartRel( event )
     circle1.radius = circle1.radius + 1
 end
 
