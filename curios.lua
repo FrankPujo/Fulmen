d46baf75d529df52f7f4ea0ca53ee134f47384c6
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2020-11-17 18:05:24 +0100

    Added sqlite3 API features

--- curios.lua
+++ curios.lua
@@ -1,6 +1,8 @@
 local composer = require ("composer")
 local scene = composer.newScene( curios )
 
+local sqlite3 = require ( "sqlite3")
+
 local math = require ("math")
 
 local sentences = require ("sentences.sql")
@@ -9,18 +11,30 @@ local rm = math.random( 4 )
 
 local sentence = display.newText( "Trivia!", 100, 100, 40 )
 
+local path = system.pathForFile( "sentences.sql", system.DocumentsDirectory )
+local db = sqlite3.open( path ) 
+
+local s1 = [[SELECT sentence FROM sentences
+    WHERE id = 1]]
+
+local s2 = [[SELECT sentence FROM sentences
+    WHERE id = 2]]
+
+local s3 = [[SELECT sentence FROM sentences
+    WHERE id = 3]]
+
 local function picking()
     if rm = 1 then
-        sentence.text = ..
+        sentence.text = s1
     end
     if rm = 2 then
-        sentence.text = ..
+        sentence.text = s2
     end
     if rm = 3 then
-        sentence.text = ..
+        sentence.text = s3
     end
     if rm = 4 then
-        sentence.text = ..
+        sentence.text = s4
     end
 end
 
@@ -29,7 +43,7 @@ local sentBtn = widget.newButton( )
 local function onSentRel( listener )
     if press = true then
         picking()
-    elseif
-        picking() = false
+    else
+        print8 "something went wrong... dang it!"
     end
 end
