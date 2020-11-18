f2b81f84bdd3ee147de03d01b0474c220e9cf2dd
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2020-11-18 09:24:02 +0100

    Fixed Loading Screen on main.lua
    The Loading Screen (also available on LoadingScreen.lua) is a sperimental code that I wrote.
    I'm still not sure of its actual functionality, so it is just a prototype.

--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
 local composer = require ( "composer" )
-composer.gotoScene( menu )
+
+local menu = composer.getScene( menu )
+local calculator = composer.getScene( calculator )
+local curios = composer.getScen( curios )
+local premenu = composer.getScene( premenu )
 
 local function startApp()
     if {
-        composer.getScene( menu ) ~= nil
-        composer.getScene( calculator ) ~= nil
-        composer.getScene( curios ) ~= nil
-        composer.getScene( premenu ) ~= nil
-    } then
+        menu ~= nil
+        calculator ~= nil
+        curios ~= nil
+        premenu ~= nil
+        } then
         composer.gotoScene( premenu )
     end
 end
